d15c61b86a3d865a6060b7aeaa72f0edd34b3cf3
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-02-17 03:17:41 -0300

    CHG: more fields to models, description & restrictions.

--- fquest/apps/event/models.py
+++ fquest/apps/event/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+"""
+Description: Class models for application 'event'.
+"""
+__author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
+
+
+from django.db import models
+from django.utils.translation import ugettext as _
+
+
+class Event(models.Model):
+    """
+    Describes an occurred event in the past.
+    """
+    occurred_in = models.DateTimeField(_("Occurred in"))
+    type = models.ForeignKey('Type', related_name=_('Type'))
+    description = models.TextField(_('What happened?'), help_text=_("Explain "
+            "what happened in that event."))
+
+    def __unicode__(self):
+        return u"<Event id=%s occured_in='%s' type=%s>" % (repr(self.id),
+                self.occurred_in, self.type.name)
+
+
+class Type(models.Model):
+    """
+    Defines an event type.
+    """
+    name = models.CharField(_('Name'), unique=True, max_length=50)
+    description = models.CharField(_('Description'), max_length=100)
+
+    def __unicode__(self):
+        return u"%s" % self.name
