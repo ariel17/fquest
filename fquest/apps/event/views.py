7c686ea3b2cae6c7b677db8cf561c7081e632e1e
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-02-20 00:03:49 -0300

    ADD: file header.

--- fquest/apps/event/views.py
+++ fquest/apps/event/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Description: View definitions for application 'event'.
+"""
+__author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
