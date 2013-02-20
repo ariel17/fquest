e7ae8ea4b638495eb277346b22a2c0c54c23f712
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-02-20 00:40:50 -0300

    ADD: filtering by family for chart.

--- fquest/apps/family/views.py
+++ fquest/apps/family/views.py
@@ -2,16 +2,19 @@
 # -*- coding: utf-8 -*-
 
 """
-Description: View definitions for application 'person'.
+Description: View definitions for application 'family'.
 """
 __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 
 
 from django.shortcuts import render_to_response
+from models import Person
 
 
-def tree(request):
+def tree(request, family_id):
     """
     Returns the necessary information to create a genealogy tree representation.
     """
-    return render_to_response('tree.html')
+    family = Person.objects.filter(family__id=family_id)
+
+    return render_to_response('tree.html', {'family': family})
