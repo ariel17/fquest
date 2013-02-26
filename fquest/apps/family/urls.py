aa2d48c9b1143bc80c32fa83c8cbefed1eee511c
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-02-26 15:12:34 -0300

    ADD: URL for detail and list pages.

--- fquest/apps/family/urls.py
+++ fquest/apps/family/urls.py
@@ -8,19 +8,32 @@ __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 
 
 from django.conf.urls import patterns, url
-from django.views.generic.list_detail import object_detail
-from models import Person
-from views import tree_chart
+from django.views.generic import list_detail
+from models import Family, Person
+from views import tree_chart, family_detail
 
 
 person_info = {
     'queryset': Person.objects.all(),
 }
 
+family_info = {
+    'queryset': Family.objects.all(),
+}
+
 urlpatterns = patterns('fquest.apps.family',
 
-        url(r'^person/(?P<object_id>\d+)/$', object_detail, person_info,
-            name='person_show'),
+        url(r'^family/$', list_detail.object_list, family_info,
+            name='family_list'),
+
+        url(r'^family/(?P<object_id>\d+)/$', family_detail,
+            name='family_detail'),
+
+        url(r'^person/$', list_detail.object_list, person_info,
+            name='person_list'),
+
+        url(r'^person/(?P<object_id>\d+)/$', list_detail.object_detail,
+            person_info, name='person_detail'),
 
         url(r'^tree/(?P<family_id>\d+)/chart/$', tree_chart,
             {'restricted': False}, name='tree_chart_complete'),
