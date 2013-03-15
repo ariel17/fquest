c741c731bc43a73ff317075b9cae575d1cefcf9e
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-03-14 23:17:22 -0300

    ADD: URL for add and editing Family and Person objects.

--- fquest/apps/family/urls.py
+++ fquest/apps/family/urls.py
@@ -8,6 +8,7 @@ __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 
 
 from django.conf.urls import patterns, url
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import create_update
 from django.views.generic import list_detail
 from models import Family, Person
@@ -22,12 +23,9 @@ family_info = {
     'queryset': Family.objects.all(),
 }
 
-
-
 urlpatterns = patterns('fquest.apps.family',
 
-        url(r'^family/add/$', create_update.create_object, {'model': Family,
-            'template_name': 'form.html', 'extra_context': {'model': Family}}, name='family_add'),
+        # Family URL
 
         url(r'^family/$', list_detail.object_list, family_info,
             name='family_list'),
@@ -35,11 +33,37 @@ urlpatterns = patterns('fquest.apps.family',
         url(r'^family/(?P<object_id>\d+)/$', family_detail,
             name='family_detail'),
 
+        url(r'^family/add/$', create_update.create_object, {'model': Family,
+            'template_name': 'form.html', 'extra_context': {'model': Family},
+            'post_save_redirect': reverse_lazy('family_list')},
+            name='family_add'),
+
+        url(r'^family/edit/(?P<object_id>\d+)/$', create_update.update_object,
+            {'model': Family, 'template_name': 'form.html',
+                'extra_context': {'model': Family},
+                'post_save_redirect': reverse_lazy('family_list')},
+            name='family_edit'),
+
+        # Person URL
+
         url(r'^person/$', list_detail.object_list, person_info,
             name='person_list'),
 
         url(r'^person/(?P<object_id>\d+)/$', list_detail.object_detail,
             person_info, name='person_detail'),
+
+        url(r'^person/add/$', create_update.create_object, {'model': Person,
+            'template_name': 'form.html',
+            'post_save_redirect': reverse_lazy('person_list')},
+            name='person_add'),
+
+        url(r'^person/edit/(?P<object_id>\d+)/$', create_update.update_object,
+            {'model': Person, 'template_name': 'form.html',
+                'extra_context': {'model': Person},
+                'post_save_redirect': reverse_lazy('person_list')},
+            name='person_edit'),
+
+        # Tree URL
 
         url(r'^tree/(?P<family_id>\d+)/chart/$', tree_chart,
             {'restricted': False}, name='tree_chart_complete'),
@@ -47,6 +71,5 @@ urlpatterns = patterns('fquest.apps.family',
         url(r'^tree/(?P<family_id>\d+)/chart/restricted/$', tree_chart,
             {'restricted': True}, name='tree_chart_restricted'),
 )
-
 
 # vim:ft=python ts=4 tw=80 cc=+1:
