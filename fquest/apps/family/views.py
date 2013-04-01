27fcd60f194884ccabf78c441ba95e2bdca7d8c6
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-04-01 00:13:53 -0300

    ADD: comment for future implementation of login_required FIX: PEP8 safe.

--- fquest/apps/family/views.py
+++ fquest/apps/family/views.py
@@ -7,11 +7,12 @@ Description: View definitions for application 'family'.
 __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 
 
+# from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from models import Family, Person
 from django.views.generic.base import View
+from models import Family, Person
 
 
 class TreeChartView(View):
@@ -37,13 +38,13 @@ class TreeChartView(View):
         family_id = kwargs['family_id']
 
         family = get_object_or_404(Family, id=family_id)
-                                                                                   
+
         persons = Person.objects.filter(family__id=family_id) \
                 if self.restricted else family.roots()
-                                                                                   
+
         node_template = self.NODE_RESTRICTED_TEMPLATE if self.restricted else \
                 self.NODE_COMPLETE_TEMPLATE
-                                                                                   
+
         return render_to_response(self.TREE_TEMPLATE, {
             'family': family,
             'persons': persons,
