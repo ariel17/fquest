90e2eedd0989711d858d78dc97a25f3ac297a551
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-08-09 18:50:53 -0300

    ADD: diagram application structure.

---
+++ fquest/apps/diagram/models.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Description: Model definition related to diagrams created by user.
+"""
+__author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
+
+
+from django.db import models
+from apps.family.models import Family
+
+
+NAME_FIELD_MAX_LENGH = 50
+
+
+class Diagram(models.Model):
+    """
+    Contains a family tree representation.
+    """
+
+    name = models.CharField(u'Name', max_length=NAME_FIELD_MAX_LENGH)
+    family = models.ForeignKey(Family)
+    # tree = tree_implementation
+
+    def __unicode__(self):
+        return u"<Diagram name='%s' family=%s>" % (self.name, self.family)
+    
+
+# vim:ft=python ts=4 tw=80 cc=+1:
