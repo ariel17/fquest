3c49cf287562c1dbec299c75deb510634cc0cfc5
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-08-09 19:10:41 -0300

    ADD: User author foreign key.

--- fquest/apps/diagram/models.py
+++ fquest/apps/diagram/models.py
@@ -9,6 +9,7 @@ __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 
 from django.db import models
 from apps.family.models import Family
+from django.contrib.auth.models import User
 
 
 NAME_FIELD_MAX_LENGH = 50
@@ -20,6 +21,7 @@ class Diagram(models.Model):
     """
 
     name = models.CharField(u'Name', max_length=NAME_FIELD_MAX_LENGH)
+    author = models.ForeignKey(User)
     family = models.ForeignKey(Family)
     # tree = tree_implementation
 
